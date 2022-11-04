a1ac310f36d55f79f3d2e68d7c846a87e583f7b9
lewis-fowler-ada <lewis.fowler@ada.ac.uk>
2022-11-04 12:30:51 +0000

    add boundaries

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 
 num = 0
+lower_bound = 1
+upper_bound = 100
 
 
-def generate_random_number(lower_bound, upper_bound):
-  rand_num = random.randint(lower_bound, upper_bound)
-  print(rand_num)
+def generate_random_number(lb, ub):
+  rand_num = random.randint(lb, ub)
+  return (rand_num)
 
 
 def difference_from_answer(guess, answer):
+  answer = generate_random_number(lower_bound, upper_bound)]
   if guess > answer:
     print("Too high")
   elif guess < answer:
