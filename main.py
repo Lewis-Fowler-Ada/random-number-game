6db982806d8bf3e107456c35761b6d8ab5439348
lewis-fowler-ada <lewis.fowler@ada.ac.uk>
2022-11-04 12:24:42 +0000

    added guessing functionality

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def generate_random_number(lower_bound, upper_bound):
   print(rand_num)
 
 
-def difference_from_answer():
-  pass
+def difference_from_answer(guess, answer):
+  if guess > answer:
+    print("Too high")
+  elif guess < answer:
+    print("Too Low")
+  else:
+    print("Correct")
